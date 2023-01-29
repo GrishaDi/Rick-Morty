35ae13c23807dc7bb6fdf4550878274252fce501
Gregory Diehl <grishadil.spb@gmail.com>
2023-01-29 10:14:22 +0300

    Init

---
+++ Rick&Morty/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Grisha Diehl on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow 
+    }
+
+
+}
+
