c4ad8af52d1e168a1ad29c68a6f73ea48d4d260a
Gregory Diehl <grishadil.spb@gmail.com>
2023-01-29 13:43:35 +0300

    Basic files for API request

---
+++ Rick&Morty/APIClient/RMEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  RMEndpoint.swift
+//  Rick&Morty
+//
+//  Created by Grisha Diehl on 29.01.2023.
+//
+
+import Foundation
+
+/// Represents unique API endpoints
+@frozen enum Endpoint: String {
+    /// Endpoint to get character info
+    case character
+    /// Endpoint to get location info
+    case location
+    /// Endpoint to get episode info
+    case episode
+}
---
+++ Rick&Morty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  Rick&Morty
+//
+//  Created by Grisha Diehl on 29.01.2023.
+//
+
+import Foundation
+
+/// Object that represents single API call
+final class RMRequest {
+    
+}
